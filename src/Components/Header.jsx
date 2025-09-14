8d46a169192e224b9a450158e03e37169ff0a11d
MuhammadSaim32 <muhammad.saim.saleem.3.8@gmail.com>
2025-09-14 17:40:41 +0500

    added responsiveness in header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useFetcher, useNavigate } from "react-router-dom";
 import Auth from "../Appwrite/Auth";
 import { Logout } from "../Store/AuthSlice";
 import { useDispatch } from "react-redux";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   const Navigate = useNavigate();
   const Dispatch = useDispatch();
+  const [DropDownNav, setDropdownNav] = useState(false);
   const AuthStatus = useSelector((state) => state.Auth.Status);
   function LogoutBtn() {
     Auth.Logout().then(() => {
       Dispatch(Logout());
       Navigate("/");
     });
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        setDropdownNav(false);
+      }
+    });
+  }, []);
+
+  function dropdown(e) {
+    e.stopPropagation();
+
+    setDropdownNav(!DropDownNav);
+  }
+
+  function removeDropDownWhenclickonlink(item) {
+    if (window.innerWidth < 768) {
+      if (item.name == "logout") {
+        LogoutBtn();
+      }
+      setDropdownNav(!DropDownNav);
+    }
   }
 
   const Items = [
@@ -42,37 +67,61 @@ const Header = () => {
       path: "/AllPost",
       AuthStatus: AuthStatus,
     },
+    {
+      name: "logout",
+      path: "",
+      AuthStatus: !AuthStatus,
+    },
   ];
 
   return (
     <header className="bg-gray-800 text-gray-300 ">
-      <div className="container mx-auto flex items-center justify-between px-4  py-6">
-        <Link to="/" className="text-2xl font-bold">
+      <div className="container mx-auto flex items-center justify-between px-4  py-6 relative">
+        <Link to="/" className="text-[1.4rem] font-bold">
           BlogApp
         </Link>
+        {/* nav links for big screens */}
+        <FontAwesomeIcon
+          icon={faX}
+          className={`${DropDownNav ? "cursor-pointer" : " hidden"}`}
+          onClick={dropdown}
+        />
 
-        <nav className="space-x-6">
+        <FontAwesomeIcon
+          icon={faBars}
+          className={`${DropDownNav ? "hidden" : "hover:cursor-pointer md:hidden"} `}
+          onClick={dropdown}
+        />
+        <nav
+          className={`md:block  md:space-x-6 ${DropDownNav ? "rounded-lg flex flex-col absolute right-0 top-16 bg-gray-800 w-32  items-center " : "hidden"}`}
+        >
           {Items.map((item) => {
-            return item.AuthStatus ? (
-              <NavLink
-                to={`${item.path}`}
-                className={({ isActive }) =>
-                  isActive ? "text-blue-700 font-bold" : "hover:text-white"
-                }
-                key={item.name}
-              >
-                {item.name}
-              </NavLink>
-            ) : null;
+            if (item.name != "logout") {
+              return item.AuthStatus ? (
+                <NavLink
+                  to={`${item.path}`}
+                  className={({ isActive }) =>
+                    `text-center m-4  p-2 ${isActive ? "text-blue-700 font-bold" : "hover:text-white"}`
+                  }
+                  key={item.name}
+                  onClick={removeDropDownWhenclickonlink}
+                >
+                  {item.name}
+                </NavLink>
+              ) : null;
+            } else {
+              return item.AuthStatus ? (
+                <NavLink
+                  to={`${item.path}`}
+                  className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300 mb-3 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50"
+                  key={item.name}
+                  onClick={(item) => removeDropDownWhenclickonlink(item)}
+                >
+                  {item.name}
+                </NavLink>
+              ) : null;
+            }
           })}
-          {AuthStatus && (
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50"
-              onClick={LogoutBtn}
-            >
-              Logout
-            </button>
-          )}
         </nav>
       </div>
     </header>
